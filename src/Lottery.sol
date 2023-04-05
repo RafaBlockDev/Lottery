ad897253210be0f37e8f65e5ad078aca413a48a6
Rafa <86583080+RafaBlockDev@users.noreply.github.com>
2023-04-05 07:49:29 -0600

    Fix issues (#3)
    * modules
    
    * forge install: chainlink-brownie-contracts
    
    1.2.0
    
    * forge install: solmate
    
    * forge install: forge-std
    
    v1.5.2
    
    * fix: add secure VFR from Chainlink
    
    * chore: add var to access using % 69 + 1
    
    * fix: add VRF var into random keccak
    
    * fix: limit to 10 million
    
    * fix: add winners case

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
-contract Lottery { 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+contract Lottery is VRFConsumerBaseV2 { 
     /// @dev Smart contract owner
     address public owner = payable(0xa6076e139FB8089C9d123dA690726B976E290799); 
     address public secondPrize = payable(0xF3f1cf9E7d7c306C25c18859eDF3a28D04FD1D4F);
@@ -12,11 +16,31 @@ contract Lottery {
     uint public maxTicketsPerBatch = 100;
     uint public winningNumbersCount = 6;
     uint public blocksPerDraw = 30;
+    uint public lastDrawBlock = 0;
 
     uint[6] public winningNumbers;
     uint256[] boughtTickets;
     bool winnerFound = false;
-    address public winner;
+    address[] public winner;
+
+    // VFR Chainlink var
+    bytes32 immutable s_keyHash;
+    uint256 public fee;
+    uint256 randomResult;
+    uint256 public s_requestId;
+    address s_owner;
+    uint64 immutable s_subscriptionId;
+    uint16 immutable s_requestConfirmations = 3;
+    uint32 public immutable s_numWords = 6;
+    uint32 immutable s_callbackGasLimit = 100000;
+    uint256[] public s_randomWords;
+
+    uint256[] ranNumbers;
+    
+    VRFCoordinatorV2Interface immutable COORDINATOR;
+    LinkTokenInterface immutable LINKTOKEN;
+
+    event ReturnedRandomness(uint256[] randomWords);
     
     mapping(address => uint256[]) tickets;
     mapping(uint256 => address) ticketToUser;
@@ -30,8 +54,20 @@ contract Lottery {
         _;
     }
     
-    constructor() payable { 
-        require(msg.value == 0, "Do not send ETH directly to this contract");
+    /**
+     * @dev Set VRF COORDINATOR, LINK, KEY HASH, VRF CONSUMER from Matic chainlink adderesses
+     */
+    constructor(
+        uint64 subscriptionId,
+        address vrfCoordinator,
+        address link,
+        bytes32 keyHash
+        ) VRFConsumerBaseV2(0x8C7382F9D8f56b33781fE506E897a4F1e2d17255) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        LINKTOKEN = LinkTokenInterface(link);
+        s_keyHash = keyHash;
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
     }
 
     /**
@@ -53,45 +89,74 @@ contract Lottery {
         return _numTickets;
     }
 
-    function test() public {
-        uint256[6] memory numbers;
-        for(uint i = 0; i < 6; i++) {
-            uint256 random = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, block.coinbase))) % 69 + 1;
-            numbers[i] =  random;
-        }
-        winningNumbers = numbers;
-    }
-
     function drawLottery() internal onlyReachBalance {
         uint256 long =  boughtTickets.length;
         require(long > 0, "not tickets bought");
         uint256[6] memory numbers;
         uint256 n = 30;
         uint256 blockNumber = block.number;
-        while(!winnerFound) {
-            uint256 random = uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, block.coinbase))) % 69 + 1;
+        uint256 counter = 0;
+        uint256 limitNum = 10e18;
+        while(winner.length < 1 && counter < limitNum) {
+            uint256 random = uint256(keccak256(abi.encodePacked(s_randomWords, block.prevrandao, block.timestamp))) % 69 + 1;
             for(uint i = 0; i < 6; i++) {
                 numbers[i] =  random;
             }
-        
-        winningNumbers = numbers;
-        uint256 selectRn = boughtTickets[random];
-        winner = ticketToUser[selectRn];
-            if(winner != address(0)) {
-                winnerFound = true;
-            } else {
-                blockNumber += n;
-                while(block.number < blockNumber) {}
-            }
-        }
-        winnerFound = true;
 
-        (bool txn1, ) = payable(winner).call{value: jackpotAmount }("");
+            uint256 selectRn = boughtTickets[random];
+            address winnerAddress = ticketToUser[selectRn];
+            if(winnerAddress != address(0)) {
+                    bool alreadyAdded = false;
+                    for(uint i = 0; i < winner.length; i++) {
+                        if(winner[i] == winnerAddress) {
+                            alreadyAdded = true;
+                            break;
+                        }
+                    }
+                    if(!alreadyAdded) {
+                        winner.push(winnerAddress);
+                    }
+                } else {
+                    blockNumber += n;
+                    while(block.number < blockNumber) {}
+                }
+                counter++;
+            }
+        require(winner.length > 0, "no winners found");
+        uint selectWinnerIndex = uint(keccak256(abi.encodePacked(s_randomWords, block.timestamp))) % winner.length;
+        address selectWinner = winner[selectWinnerIndex];
+        (bool txn1, ) = payable(selectWinner).call{value: jackpotAmount }("");
         require(txn1, "transaction not executed to winner");
         (bool tx2, ) = payable(owner).call{value: 95 ether }("");
         require(tx2, "transaction not executed to owner");
         (bool tx3, ) = payable(secondPrize).call{value: 5 ether }("");
         require(tx3, "transaction not executed to ");
+    }
+
+    function getWinnerTickets(uint256[6] memory _numbers) internal view returns(uint256[] memory) {
+
+    }
+
+    /*****************************************/
+    /************* VFR FUNCTIONS *************/
+    /*****************************************/
+
+    function requestRandomWords() external {
+        s_requestId = COORDINATOR.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            s_requestConfirmations,
+            s_callbackGasLimit,
+            s_numWords
+        );
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        s_randomWords = randomWords;
+        emit ReturnedRandomness(randomWords);
     }
 
     /*******************************/
